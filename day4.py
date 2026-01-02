25c418f23def0212543313df8538fb521537082e
Dragoş <edragos33@gmail.com>
2026-01-02 11:26:06 +0100

    ex 2 done

--- day4.py
+++ day4.py
@@ -1,4 +1,12 @@
 # --- Day 4: 30 Days of python programming ---
 
+# 1. Concatenate the string 'Thirty', 'Days', 'Of', 'Python' to a single string, 'Thirty Days Of Python'.
+"""
 string = "Thirty " + "Days " + "Of " + "Python"
 print(string)
+"""
+
+# 2. Concatenate the string 'Coding', 'For' , 'All' to a single string, 'Coding For All'.
+coding_string = ['Coding', 'For' , 'All']
+result = ' '.join(coding_string)
+print(result)
