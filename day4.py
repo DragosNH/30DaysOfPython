75f4578a32e9916f03d02e7dc7d6cacf0280b087
Dragoş <edragos33@gmail.com>
2026-01-02 11:29:35 +0100

    ex 3 to 6 done

--- day4.py
+++ day4.py
@@ -7,6 +7,21 @@ print(string)
 """
 
 # 2. Concatenate the string 'Coding', 'For' , 'All' to a single string, 'Coding For All'.
+"""
 coding_string = ['Coding', 'For' , 'All']
 result = ' '.join(coding_string)
 print(result)
+"""
+# 3. Declare a variable named company and assign it to an initial value "Coding For All".
+
+company = "Coding For All"
+
+# 4. Print the variable company using print().
+print(company)
+
+# 5. Print the length of the company string using len() method and print().
+print(len(company))
+
+# 6. Change all the characters to uppercase letters using upper() method.
+
+print(company.upper())
