0085549affeaa0da6d5431a02d723a56ae3ff70c
Dragoş <edragos33@gmail.com>
2026-01-03 06:04:47 +0100

    ex 1 to 5 done

---
+++ day5.py
@@ -0,0 +1,20 @@
+# --- Day 4: 30 Days of python programming ---
+
+# 1. Declare an empty list
+empty_list = []
+
+# 2. Declare a list with more than 5 items
+random_items_list = ["lemon", "cookies", "bread", "chair", "remote", "dog", "bird person"]
+
+# 3.Find the length of your list
+# print(len(random_items_list))
+
+# 4.Get the first item, the middle item and the last item of the list
+"""
+print(random_items_list[0])
+print(random_items_list[3])
+print(random_items_list[6])
+"""
+
+# 5. Declare a list called mixed_data_types, put your(name, age, height, marital status, address)
+mixed_data_types = ["Dragos", 3000, 1.75, "yes", "next to my neighbour"]
