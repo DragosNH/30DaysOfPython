504d69a8f47634ba5cdb18e234ac306df23d4bcd
Dragoş <edragos33@gmail.com>
2026-01-03 06:09:07 +0100

    ex 6 to 10 done

--- day5.py
+++ day5.py
@@ -18,3 +18,23 @@ print(random_items_list[6])
 
 # 5. Declare a list called mixed_data_types, put your(name, age, height, marital status, address)
 mixed_data_types = ["Dragos", 3000, 1.75, "yes", "next to my neighbour"]
+
+# 6. Declare a list variable named it_companies and assign initial values Facebook, Google, Microsoft, Apple, IBM, Oracle and Amazon.
+it_companies = ["Facebook", "Google", "Microsoft", "Apple", "IBM", "Oracle", "Amazon"]
+
+# 7. Print the list using print()
+# print(it_companies)
+
+# 8. Print the number of companies in the list
+# print(len(it_companies))
+
+# 9. Print the first, middle and last company
+"""
+print(it_companies[0])
+print(it_companies[3])
+print(it_companies[6])
+"""
+
+# 10. Print the list after modifying one of the companies
+it_companies[0] = "Meta"
+print(it_companies)
