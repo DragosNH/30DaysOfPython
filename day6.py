1ae7e280809bffa07f3f596e07ac89975bc2673b
Dragoş <edragos33@gmail.com>
2026-01-04 06:29:55 +0100

    ex 1 to 5 done

--- day6.py
+++ day6.py
@@ -19,3 +19,25 @@ print(len(siblings))
 parents = ("Mom", "Dad")
 
 family_members = siblings + parents
+
+# --- Level 2 ---
+
+# 1. Unpack siblings and parents from family_members
+bro1, bro2, bro3, sis1, sis2, sis3, mom, dad = family_members
+
+# 2. Create fruits, vegetables and animal products tuples. Join the three tuples and assign it to a variable called food_stuff_tp.
+fruits = ("Banana", "Apple", "Orange", "Kiwi")
+vegetables = ("Potato", "Carrot", "Cuncumber", "Onion")
+animal_products = ("Cheese", "Eggs", "Milk", "Yogurt")
+
+food_stuff_tp = fruits + vegetables + animal_products
+
+# 3. Change the about food_stuff_tp tuple to a food_stuff_lt list
+food_stuff_lt = list(food_stuff_tp)
+
+# 4. Slice out the middle item or items from the food_stuff_tp tuple or food_stuff_lt list.
+print(food_stuff_lt[6])
+
+# 5. Slice out the first three items and the last three items from food_staff_lt list
+print(food_stuff_lt[:3])
+print(food_stuff_lt[-3:])
