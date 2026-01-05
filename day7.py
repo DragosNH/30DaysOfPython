b0a536f7554369e0653dd064a4b16a87c133d558
Dragoş <edragos33@gmail.com>
2026-01-05 09:21:45 +0100

    Level 2 done

--- day7.py
+++ day7.py
@@ -22,3 +22,29 @@ it_companies.remove('Facebook')
 """
 remove() reises a KeyError if the element is not found while discard() does not rise an error.
 """
+
+# --- Level 1 ---
+# 1.Join A and B
+C = A.union(B)
+
+# 2.Find A intersection B
+# print(A.intersection(B)) #{19, 20, 22, 24, 25, 26}
+
+# 3. Is A subset of B
+# print("A is subset of B", A.issuperset(B)) #False
+
+# 4. Are A and B disjoint sets
+# print("A is disjount of B", A.isdisjoint(B)) #False
+
+# 5. Join A with B and B with A
+D = A.union(B)
+E = B.union(A)
+F = D.union(E)
+# print(F)
+
+# 6. What is the symmetric difference between A and B
+# print(A.symmetric_difference(B)) #{27, 28}
+
+# 7. Delete the sets completely
+del A
+del B
