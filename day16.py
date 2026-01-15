c82040000d1b12108aa3ce5c1a79777267ef43de
Dragoş <edragos33@gmail.com>
2026-01-15 06:22:03 +0100

    ex 3 and 4 done

--- day16.py
+++ day16.py
@@ -7,4 +7,14 @@ now = datetime.now()
 
 # 2. Format the current date using this format: "%m/%d/%Y, %H:%M:%S")
 formated_now_1 = now.strftime("%m/%d/%Y, %H:%M:%S")
-print(formated_now_1)
+# print(formated_now_1)
+
+# 3. Today is 5 December, 2019. Change this time string to time.
+date_string = "5 December, 2019"
+date_object = datetime.strptime(date_string, "%d %B, %Y")
+# print(date_object)
+
+# 4. Calculate the time difference between now and new year.
+new_year = datetime.strptime("1 January, 2027", "%d %B, %Y")
+time_difference = new_year - now
+# print(time_difference)
