bd29b3671cf9d32a4bcd13ee3712694b1d5828a3
Dragoş <edragos33@gmail.com>
2026-01-15 06:14:45 +0100

    ex 1 and 2 done

--- day16.py
+++ day16.py
@@ -1,1 +1,10 @@
 # --- Day 16: 30 Days of python programming ---
+# 1. Get the current day, month, year, hour, minute and timestamp from datetime module
+from datetime import datetime
+
+now = datetime.now()
+# print(now)
+
+# 2. Format the current date using this format: "%m/%d/%Y, %H:%M:%S")
+formated_now_1 = now.strftime("%m/%d/%Y, %H:%M:%S")
+print(formated_now_1)
