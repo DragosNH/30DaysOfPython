db86ffafcadcc81917bce14e71d9818caa98505c
Dragoş Enache <155900245+DragosNH@users.noreply.github.com>
2026-01-24 06:06:42 +0100

    ex 1 and 2 done

--- day25.py
+++ day25.py
@@ -1,1 +1,13 @@
 # --- Day 25: 30 Days of python programming ---
+import pandas as pd
+import numpy as np
+
+
+data = {
+    'Name': ['John', 'Bob', 'George', 'Janne', 'Milly', 'Rebecca'],
+    'Country': list(['Romania', 'France', 'Marroco', 'Vietnam', 'Egypt', 'US']),
+    'City': list(['Ploiesti', 'Mulhouse', 'Casablanca', 'Hanoi', 'Cairo', 'Denver'])
+}
+
+df = pd.DataFrame(data, columns=['Name','Country','City'])
+print(df[:5])
